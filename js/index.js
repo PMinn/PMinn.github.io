78b6dc5359e191268470628d53b42e713d1c15d5
PMinn <40393799+PMinn@users.noreply.github.com>
2024-01-21 01:53:09 +0800

    Update

--- js/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        var loading = document.getElementById('loading');
-        loading.style.opacity = '0';
-        setTimeout(() => loading.style.display = 'none', 1000);
-    }, 1000);
-});
