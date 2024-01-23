8c2ba530fc39b712d2de20f9499d1f91721f789a
PMinn <40393799+PMinn@users.noreply.github.com>
2024-01-23 22:53:39 +0800

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'export',
-  distDir: 'out',
-  basePath: '/github-pages',
 };
 
 export default nextConfig;
