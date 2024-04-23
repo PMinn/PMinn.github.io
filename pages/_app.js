b615580a71ece19b7e6101e31a8b88aea1c4fd6d
PMinn <40393799+PMinn@users.noreply.github.com>
2024-04-23 17:11:49 +0800

    add: favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { useRef } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import { Dekko } from 'next/font/google';
 import 'react-multi-carousel/lib/styles.css';
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
@@ -35,6 +36,12 @@ export default function App({ Component, pageProps }) {
         onLocationChange={scroll => scroll.scrollTo(0, { duration: 0, disableLerp: true })} // If you want to reset the scroll position to 0 for example
     // onUpdate={() => console.log('Updated, but not on location change!')} // Will trigger on 
     >
+        <Head>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+        </Head>
         <main data-scroll-container ref={containerRef} style={{ fontFamily: `${dekko.style.fontFamily}, naikaifont, "Noto Sans TC", Arial, Helvetica, sans-serif` }}>
             <Component {...pageProps} />
         </main>
